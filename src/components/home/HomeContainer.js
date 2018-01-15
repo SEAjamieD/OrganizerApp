b1dce60a85134fba05293a5a9c96459890dd24f4
SEAjamieD <jamie.degnan@gmail.com>
2018-01-14 19:12:44 -0800

    Beginning firebase initialization, restrict keys til i hide them

--- src/components/home/HomeContainer.js
+++ src/components/home/HomeContainer.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import { View, StyleSheet, Text, TextInput, Dimensions, TouchableHighlight, Image } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 
+// Initialize Firebase
+import * as firebase from 'firebase';
+// FIREBASE INITIALIZER HERE
+// const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+
 import Plus from './Plus';
 import Search from './Search';
 import House from './House';
